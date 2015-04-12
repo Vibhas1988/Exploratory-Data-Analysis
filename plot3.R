811cbd4562ed68f8b36ac2b766c84d74851d0027
Vibhas1988 <trivedivibhas@gmail.com>
2015-04-12 09:47:44 +0530

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -1,26 +1,13 @@
-## Getting full dataset
-data_full <- read.csv("./Data/household_power_consumption.txt", header=T, sep=';', na.strings="?", 
-                      nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
-data_full$Date <- as.Date(data_full$Date, format="%d/%m/%Y")
-
-## Subsetting the data
-data <- subset(data_full, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
-rm(data_full)
-
-## Converting dates
-datetime <- paste(as.Date(data$Date), data$Time)
-data$Datetime <- as.POSIXct(datetime)
-
-## Plot 3
-with(data, {
-    plot(Sub_metering_1~Datetime, type="l",
-         ylab="Global Active Power (kilowatts)", xlab="")
-    lines(Sub_metering_2~Datetime,col='Red')
-    lines(Sub_metering_3~Datetime,col='Blue')
-})
-legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, 
-       legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
-
-## Saving to file
-dev.copy(png, file="plot3.png", height=480, width=480)
+data <- read.table(file="C:/Users/vibhas/R/exdata_data_household_power_consumption/household_power_consumption.txt", header=TRUE,sep=";")
+subsetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+datetime <- strptime(paste(subsetData$Date, subsetData$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+globalActivePower <- as.numeric(subsetData$Global_active_power)
+subMetering1 <- as.numeric(subsetData$Sub_metering_1)
+subMetering2 <- as.numeric(subsetData$Sub_metering_2)
+subMetering3 <- as.numeric(subsetData$Sub_metering_3)
+png("plot3.png", width=480, height=480)
+plot(datetime, subMetering1, type="l", ylab="Energy Submetering", xlab="")
+lines(datetime, subMetering2, type="l", col="red")
+lines(datetime, subMetering3, type="l", col="blue")
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, lwd=2.5, col=c("black", "red", "blue"))
 dev.off()
